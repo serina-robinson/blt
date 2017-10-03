10c1c8c13315dd7be647a523e02b9fc9cc31ee9b
Serina Robinson <robi0916@umn.edu>
2017-10-03 15:30:58 -0500

    updated browse and database pages

--- build_site.r
+++ build_site.r
@@ -2,7 +2,11 @@
 #This helps avoid confusion if our working directory is 
 #not our site because of other projects we were 
 #working on at the time. 
-setwd("~/Documents/University_of_Minnesota/Wackett_Lab/github/Blt/")
+#Install packages
+packs<-c("dplyr","tidyr")
+lapply(packs, require, character.only=T)
+
+setwd("~/Documents/University_of_Minnesota/Wackett_Lab/github/blt/")
 
 #render your sweet site. 
 rmarkdown::render_site()
---
+++ src/gene_plot.r
@@ -0,0 +1,44 @@
+#Install packages
+#install.packages("genoPlotR")
+#require(genoPlotR)
+gene_plot<-function(fil){
+  #Read in the get_dna_segs function
+  source("src/get_dna_segs.R")
+  
+  #Read in the cystargolide gene cluster
+  cys<-readAAStringSet(fil)
+  ll<-list(cys)
+  
+  dna<-get_dna_segs(ll)
+  dnal<-list(dna)
+  dnal[[1]]$strand<-(-1)
+  dnal[[1]]$col<-c("gray","orange","slateblue4","red","red","forestgreen","red","hotpink","slateblue4","gray")
+  #dnal[[1]]$col<-c("white","gray","dodgerblue","dodgerblue","red","dodgerblue","red","dodgerblue","gray","white")
+  
+  dnal[[1]]$lwd[7]<-3
+  
+  mid_pos <- middle(dnal[[1]])
+  mid_pos
+  lett<-c("orf-1",toupper(letters[1:8]),"orf+1")
+  annot<-genoPlotR::annotation(x1=mid_pos,
+                               text=lett,
+                               rot=0, col="black")
+  
+  pl<-plot_gene_map(dna_segs = dnal,gene_type="arrows",
+                    annotation_height=-20,
+                    annotations=annot)
+  
+  
+  colors<-unique(dnal[[1]]$col)[2:length(unique(dnal[[1]]$col))]
+  #CysA = orange= 2-IPM synthase, IPM biosynthesis
+  #CysB, H = slateblue4 = regulatin/transport
+  #CysC, CysD, CysF -> red = AMP-dependent synthetase/ligase
+  #CysE -> methylesterase
+  #CysG -> methyltransferase
+  
+  proteins<-c("2-IPM synthase","regulation/transport","ANL superfamily protein","methylesterase","methyltransferase")
+  plot.new()
+  leg<-legend("bottom",legend=proteins,fill = colors,bty="n")
+  
+  return(list(pl,leg))
+}
